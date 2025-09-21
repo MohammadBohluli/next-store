f6c8d953d07589787423ff9cede663f96bb5de95
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2025-09-21 15:39:52 +0330

    create page product details

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -3,12 +3,17 @@ import { LATEST_PRODUCTS_LIMIT } from "../constants";
 import { mapToPlainObj } from "../utils";
 
 export async function getLatestProducts() {
-  // const db = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
   return mapToPlainObj(data);
+}
+
+export async function getProductBySlug(slug: string) {
+  const product = await prisma.product.findFirst({
+    where: { slug },
+  });
+  return product;
 }
