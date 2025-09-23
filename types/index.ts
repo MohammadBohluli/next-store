11b0dfbb1d00c4227ca0e67c23d2f3a2986ff20a
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2025-09-23 13:44:47 +0330

    create zod type cart and cart prisma model

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,8 @@
-import { createProductSchema } from "@/lib/validators";
+import {
+  cartItemSchema,
+  createCartSchema,
+  createProductSchema,
+} from "@/lib/validators";
 import { z } from "zod";
 
 export type Product = z.infer<typeof createProductSchema> & {
@@ -6,3 +10,6 @@ export type Product = z.infer<typeof createProductSchema> & {
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof createCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
