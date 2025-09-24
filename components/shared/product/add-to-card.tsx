b076289472831c18ba317d82b1705e7d7b45f25c
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2025-09-24 11:39:24 +0330

    implement remove item from cart

--- components/shared/product/add-to-card.tsx
+++ components/shared/product/add-to-card.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { addItemToCart } from "@/lib/actions/cart.action";
-import { CartItem } from "@/types";
-import { Plus } from "lucide-react";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.action";
+import { Cart, CartItem } from "@/types";
+import { Minus, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 interface Props {
   item: CartItem;
+  cart?: Cart;
 }
 
-const AddToCart = ({ item }: Props) => {
+const AddToCart = ({ item, cart }: Props) => {
   const router = useRouter();
+
   const handleAddToCart = async () => {
     const resp = await addItemToCart(item);
 
@@ -27,7 +29,26 @@ const AddToCart = ({ item }: Props) => {
     });
   };
 
-  return (
+  const handleRemoveFromCart = async () => {
+    const resp = await removeItemFromCart(item.productId);
+
+    resp.success ? toast.success(resp.message) : toast.error(resp.message);
+  };
+
+  const existItem =
+    cart && cart.items.find((itm) => itm.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2"> {existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
       <Plus /> Add to cart
     </Button>
--- lib/actions/cart.action.ts
+++ lib/actions/cart.action.ts
@@ -39,7 +39,7 @@ export async function addItemToCart(data: CartItem) {
       });
 
       await prisma.cart.create({ data: newCart });
-      revalidatePath(`/product/${prodcut.slug}`);
+      revalidatePath(`/products/${prodcut.slug}`);
       return { success: true, message: `Item ${prodcut.name} add to cart` };
     } else {
       // check item is already in cart
@@ -68,7 +68,7 @@ export async function addItemToCart(data: CartItem) {
         where: { id: cart.id },
         data: { items: cart.items, ...calcPrice(cart.items) },
       });
-      revalidatePath(`/product/${prodcut.slug}`);
+      revalidatePath(`/products/${prodcut.slug}`);
 
       return {
         success: false,
@@ -107,4 +107,58 @@ export async function getMyCart() {
     shippingPrice: cart.shippingPrice.toString(),
     taxPrice: cart.taxPrice.toString(),
   });
+}
+
+export async function removeItemFromCart(productId: string) {
+  try {
+    // Get session cart id
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    if (!sessionCartId) throw new Error("Cart Session not found");
+
+    // Get product
+    const product = await prisma.product.findFirst({
+      where: { id: productId },
+    });
+    if (!product) throw new Error("Product not found");
+
+    // Get user cart
+    const cart = await getMyCart();
+    if (!cart) throw new Error("Cart not found");
+
+    // Check if cart has item
+    const exist = cart.items.find((x) => x.productId === productId);
+    if (!exist) throw new Error("Item not found");
+
+    // Check if cart has only one item
+    if (exist.qty === 1) {
+      // Remove item from cart
+      cart.items = cart.items.filter((x) => x.productId !== exist.productId);
+    } else {
+      // Decrease quantity of existing item
+      cart.items.find((x) => x.productId === productId)!.qty = exist.qty - 1;
+    }
+
+    // Update cart in database
+    await prisma.cart.update({
+      where: { id: cart.id },
+      data: {
+        items: cart.items,
+        ...calcPrice(cart.items),
+      },
+    });
+
+    // Revalidate product page
+    revalidatePath(`/products/${product.slug}`);
+
+    return {
+      success: true,
+      message: `${product.name}  ${
+        cart.items.find((x) => x.productId === productId)
+          ? "updated in"
+          : "removed from"
+      } cart successfully`,
+    };
+  } catch (error) {
+    return { success: false, message: "item was not remove from cart" };
+  }
 }
