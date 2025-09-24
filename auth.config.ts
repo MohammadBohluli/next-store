ee9f319fa3f213c1fef954602c6a8dab9142d1a2
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2025-09-24 11:55:29 +0330

    bigfix: seperate config auth in next auth for vercel error limitation size of middleware

---
+++ auth.config.ts
@@ -0,0 +1,97 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthConfig } from "next-auth";
+
+import { compareSync } from "bcrypt-ts-edge";
+
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextResponse } from "next/server";
+import { prisma } from "./db/prisma";
+export const authConfig: NextAuthConfig = {
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: { email: { type: "email" }, password: { type: "password" } },
+      async authorize(credentials) {
+        if (credentials === null) return null;
+        const user = await prisma.user.findFirst({
+          where: { email: credentials?.email as string },
+        });
+
+        if (user && user.password) {
+          // if password is correct is Match is true
+          const isMatch = compareSync(
+            credentials?.password as string,
+            user.password
+          );
+
+          if (isMatch) {
+            return {
+              id: user.id,
+              name: user.name,
+              email: user.email,
+              role: user.role,
+            };
+          }
+        }
+        // if user does not exist or password does not match return
+        return null;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, user, trigger, token }: any) {
+      session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+
+      if (trigger === "update") {
+        session.user.name = user.name;
+      }
+      return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      if (user) {
+        token.role = user.role;
+
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // update db to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+
+      // Handle session updates (e.g., name change)
+      if (session?.user.name && trigger === "update") {
+        token.name = session.user.name;
+      }
+      return token;
+    },
+    authorized: ({ request, auth }) => {
+      // check session cart cookie
+
+      if (!request.cookies.get("sessionCartId")) {
+        const sessionCartId = crypto.randomUUID();
+        const newRequestHeaders = new Headers(request.headers);
+        const response = NextResponse.next({
+          request: { headers: newRequestHeaders },
+        });
+        response.cookies.set("sessionCartId", sessionCartId);
+        return response;
+      } else {
+        return true;
+      }
+    },
+  },
+};
