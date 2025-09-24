dbe4c394006d037f1bf998c99706775611c78c44
MohammadBohluli <mohammad.bohluli.8789@gmail.com>
2025-09-24 18:34:46 +0330

    create cart page

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { type Cart } from "@/types";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+
+interface Props {
+  cart?: Cart;
+}
+
+const CartTable = ({ cart }: Props) => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  return (
+    <>
+      <h1 className="py-4 h2-bold">Shopping Cart</h1>
+      {!cart || cart.items.length === 0 ? (
+        <div>
+          Cart is Empty. <Link href="/">Go Shopping</Link>
+        </div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">Table</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CartTable;
